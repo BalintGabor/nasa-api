aaff5164e55240b2e07127093f5b3c261ae362a8
Gabor Balint <trondheim.bg@gmail.com>
2023-12-19 14:50:35 +0100

    button eventlistener

--- frontend/script.js
+++ frontend/script.js
@@ -1,27 +1,34 @@
-const loadEvent = async function(){
+const loadEvent = async function() {
     // Selectors
-    const mediaContainerElement = document.getElementById("media-container")
-    const titleElement = document.getElementById("article-title")
-    const explanationElement = document.getElementById("article-explanation")
+    const mediaContainerElement = document.getElementById("media-container");
+    const titleElement = document.getElementById("article-title");
+    const explanationElement = document.getElementById("article-explanation");
 
     // Fetch the today's data
-    const response = await fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-    const responseJson = await response.json()
-    console.log(responseJson)
+    const response = await fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY");
+    const responseJson = await response.json();
 
     // Define the media content: image or video
-    const mediaContent = imgOrIframe ()
-    function imgOrIframe () {
+    const mediaContent = imgOrIframe();
+    function imgOrIframe() {
         if (responseJson.media_type === "video") {
-            return `<iframe src="${responseJson.url}">`
+            return `<iframe src="${responseJson.url}">`;
         }
-        return `<img src="${responseJson.hdurl}">`
+        return `<img src="${responseJson.hdurl}">`;
     }
 
     // Insert the media content, title and explanation
-    mediaContainerElement.insertAdjacentHTML("beforeend", mediaContent)
-    titleElement.insertAdjacentHTML("beforeend", responseJson.title)
-    explanationElement.insertAdjacentHTML("beforeend", responseJson.explanation)
+    mediaContainerElement.insertAdjacentHTML("beforeend", mediaContent);
+    titleElement.insertAdjacentHTML("beforeend", responseJson.title);
+    explanationElement.insertAdjacentHTML("beforeend", responseJson.explanation);
+
+    // Button eventlistener
+    const button = document.getElementById("search-button");
+    button.addEventListener("click", clickSearch);
+    async function clickSearch() {
+        let input = document.getElementById("chosen-date").value;
+        console.log(input)
+    }
 }
 
 window.addEventListener("load", loadEvent)
